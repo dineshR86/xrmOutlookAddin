7ead460cec86d3dc789fe013cbd3bad5b9fee460
dineshr86 <dinesh_gudapati@outlook.com>
2018-10-18 22:19:35 +0530

    Added search dropdown

--- XRMOutlookAddinUI/src/index.js
+++ XRMOutlookAddinUI/src/index.js
@@ -3,12 +3,21 @@
  * See LICENSE in the project root for license information.
  */
 import 'bootstrap';
-
+//import 'bootstrap-select';
 
 $(document).ready(() => {
     $('#run').click(run);
+    $.fn.selectpicker.Constructor.BootstrapVersion = '4';
+    $("#sitecollections").on("change", function (event) {
+        // if ($(this).find("option:selected").val() > 1) {
+        //     $("#lists").css("display", "block");
+        // }
+        console.log($(this).find("option:selected"));
+        $("#lists").css("display", "block");    
+    });
+
 });
-  
+
 // The initialize function must be run each time a new page is loaded
 Office.initialize = (reason) => {
     $('#sideload-msg').hide();
@@ -20,3 +29,4 @@ async function run() {
          * Insert your Outlook code here
          */
 }
+
